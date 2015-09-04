324839be04ad62fec8f8f1fa80d57382206be47c
Harald Floor Wihlelmsen <haraldfw@gmail.com>
2015-09-04 13:25:23 +0200

    fixed code, imports waiting

--- Hitra.java
+++ Hitra.java
@@ -1,30 +1,28 @@
 import lejos.nxt.*;
 import lejos.robotics.subsumption.*;
 
-
 class Hitra	{
 
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 
-	final TouchSensor trykk = new TouchSensor(SensorPort.S1);
-	final TouchSensor trykk1 = new TouchSensor(SensorPort.S2);
-	final LightSensor lys = new LightSensor(SensorPort.S3);
-	final SoundSensor lyd = new SoundSensor(SensorPort.S4);
-	int lysgrense = lys.readValue();
+		final TouchSensor trykk = new TouchSensor(SensorPort.S1);
+		final TouchSensor trykk1 = new TouchSensor(SensorPort.S2);
+		final LightSensor lys = new LightSensor(SensorPort.S3);
+		final SoundSensor lyd = new SoundSensor(SensorPort.S4);
+		int lysgrense = lys.readValue();
 
-	Motor.A.setSpeed(200);
-	Motor.B.setSpeed(200);
-	Motor.C.setSpeed(500);
+		Motor.A.setSpeed(200);
+		Motor.B.setSpeed(200);
+		Motor.C.setSpeed(500);
 
-	lys.setFloodlight(true);
+		lys.setFloodlight(true);
 
-	for(int i = 0; i <10;){
-		Motor.A.forward();
-		Motor.B.forward();
+		for(int i = 0; i <10;){
+			Motor.A.forward();
+			Motor.B.forward();
 
 			if(trykk.isPressed() || trykk1.isPressed()){
-
 				i++;
 				Sound.buzz();
 				Motor.A.backward();
@@ -35,8 +33,7 @@ class Hitra	{
 				Motor.B.backward();
 				Thread.sleep(1000);
 
-			else if(lysgrense < 40){
-
+			} else if(lysgrense < 40){
 				i++;
 				Sound.buzz();
 				Motor.A.backward();
@@ -45,8 +42,8 @@ class Hitra	{
 
 				Motor.A.forward();
 				Motor.B.backward();
-
 				Thread.sleep(1000);
-		    }
+			}
 		}
 	}
+}
