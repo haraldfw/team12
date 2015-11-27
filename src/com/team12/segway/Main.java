b26de7677856b8e34381e4afae8f8ef7cf0219eb
Harald Floor Wihlelmsen <haraldfw@gmail.com>
2015-11-27 12:17:14 +0100

    Added new code for turn-in

--- src/com/team12/segway/Main.java
+++ src/com/team12/segway/Main.java
@@ -7,9 +7,14 @@ import lejos.hardware.Button;
  */
 public class Main {
 
+    /**
+     * Initializes a segway-object and calls it's update-method as often as possible until the segway has fallen.
+     *
+     * @param args Not used
+     */
     public static void main(String[] args) {
         Segway segway = new Segway();
-        while(!Button.ENTER.isDown()) {
+        while (!Button.ENTER.isDown() && !segway.hasFallen) {
             segway.update();
         }
     }
--- src/com/team12/segway/Segway.java
+++ src/com/team12/segway/Segway.java
@@ -4,30 +4,74 @@ import lejos.hardware.BrickFinder;
 import lejos.hardware.motor.Motor;
 import lejos.hardware.motor.NXTRegulatedMotor;
 import lejos.hardware.sensor.EV3GyroSensor;
-import lejos.robotics.SampleProvider;
 
 /**
  * Created by haraldfw on 10/16/15.
  */
 public class Segway {
 
-    NXTRegulatedMotor motorLeft = Motor.A;
-    NXTRegulatedMotor motorRight = Motor.B;
-    SampleProvider gyro;
+    NXTRegulatedMotor motorRight = Motor.A;
+    NXTRegulatedMotor motorLeft = Motor.B;
+    /**
+     * The gyro-sensor to be used by the segway.
+     */
+    EV3GyroSensor gyroSensor;
+    /**
+     * Needed to read values from the sensor.
+     */
     float[] samples;
+    /**
+     * A boolean that is true once the segway has fallen.
+     */
+    boolean hasFallen = false;
 
+    /**
+     * returns a new Segway-object
+     */
     public Segway() {
-        gyro = new EV3GyroSensor(BrickFinder.getDefault().getPort("1")).getAngleMode();
-        samples = new float[gyro.sampleSize()];
+        gyroSensor = new EV3GyroSensor(BrickFinder.getDefault().getPort("S1"));
+        samples = new float[gyroSensor.getAngleMode().sampleSize()];
     }
 
-    // This method is called as often as possible. This is where the segway reacts to gyro-input
+    /**
+     * This method is called as often as possible. This is where the segway reacts to gyro-input.
+     */
     public void update() {
-        
+        float angle = readAngle();
+        float rate = readRate();
+        motorLeft.setSpeed(50 * rate);
+        motorRight.setSpeed(50 * rate);
+
+        if (angle < 50 || angle > 130) {
+            hasFallen = true;
+        }
+
+        if (angle < 90) {
+            motorLeft.backward();
+            motorRight.backward();
+        } else if (angle > 90) {
+            motorLeft.forward();
+            motorRight.forward();
+        }
     }
 
-    private float readGyro() {
-        gyro.fetchSample(samples, 0);
+    /**
+     * Returns the value for the angle of the gyrosensor.
+     *
+     * @return The angle value of the sensor in degrees.
+     */
+    private float readAngle() {
+        gyroSensor.getAngleMode().fetchSample(samples, 0);
+        return samples[0];
+    }
+
+    /**
+     * Returns the rotation-velocity of the sensor in degrees per second.
+     *
+     * @return The rotation-velocity in degrees per second.
+     */
+    private float readRate() {
+        gyroSensor.getRateMode().fetchSample(samples, 0);
         return samples[0];
     }
 }
